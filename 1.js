f6ecd421176cee06ec2e213d85f17046a3af4633
Quang <thanhmainc45@gmail.com>
2019-10-05 14:31:02 +0700

    fix animate

--- 1.js
+++ 1.js
@@ -7,7 +7,7 @@ $(function(){
 
 
 	$(".phanhoi").click(function(){
-		#('body, html').animate({scrollTop:$('.khoicomment').offset().top}, 600);
+		$('body, html').animate({scrollTop:$('#phanhoii').offset().top}, 600);
 		return false;
 	})
 

1d5d809c0b604be9284122f79db4c4c3271487f0
Quang <thanhmainc45@gmail.com>
2019-10-05 14:24:46 +0700

    them animate

---
+++ 1.js
@@ -0,0 +1,14 @@
+$(function(){
+
+	$(".kn, .sp").click(function(){
+		$('body, html').animate({scrollTop:$('#edu').offset().top}, 600);
+		return false;
+	})
+
+
+	$(".phanhoi").click(function(){
+		#('body, html').animate({scrollTop:$('.khoicomment').offset().top}, 600);
+		return false;
+	})
+
+})

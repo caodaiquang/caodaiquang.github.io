d94ec94d1b6c71c74af7f479db855f696c815649
Quang <thanhmainc45@gmail.com>
2019-10-29 18:02:25 +0700

    hwifhewi

---
+++ js/custom.js
@@ -0,0 +1,50 @@
+(function($) {
+
+  // Init Wow
+  wow = new WOW({
+    animateClass: 'animated',
+    offset: 100
+  });
+  wow.init();
+
+  $(".navbar-collapse a").on('click', function() {
+    $(".navbar-collapse.collapse").removeClass('in');
+  });
+
+  // Navigation scrolls
+  $('.navbar-nav li a').bind('click', function(event) {
+    $('.navbar-nav li').removeClass('active');
+    $(this).closest('li').addClass('active');
+    var $anchor = $(this);
+    var nav = $($anchor.attr('href'));
+    if (nav.length) {
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+      }, 700, 'easeInOutExpo');
+
+      event.preventDefault();
+    }
+  });
+   //jQuery to collapse the navbar on scroll
+  $(window).scroll(function() {
+    if ($(".navbar-default").offset().top > 80) {
+      $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+      $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+  });
+
+  // icondrop section scroll
+  $(".icondrop a").on('click', function(event) {
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function() {
+      window.location.hash = hash;
+    });
+  });
+
+ 
+
+})(jQuery);
